0e4487718b7f33279522e61f0445a408396d918c
NingShuZhu <1794617162@qq.com>
2023-12-09 18:48:26 +0000

    add javafx stuff to pom.xml and module-info

--- SnackGame/src/main/java/module-info.java
+++ SnackGame/src/main/java/module-info.java
@@ -1,4 +1,9 @@
 module com.comp2013cw.snakegame {
     requires java.desktop;
-    opens com.comp2013cw.snakegame;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.fxml;
+
+    opens com.comp2013cw.snakegame to javafx.fxml;
+    exports com.comp2013cw.snakegame;
 }
